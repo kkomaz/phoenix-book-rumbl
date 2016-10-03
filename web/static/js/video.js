e3f9133b0a8ef7709af495dbc21484de8ab6e62d
jechol <ryanrhie9@gmail.com>
2016-10-03 09:40:35 +0900

    page 181

--- web/static/js/video.js
+++ web/static/js/video.js
@@ -12,14 +12,18 @@ let Video = {
     Player.init(element.id, playerId, () => {
       this.onReady(videoId, socket)
     })
- },
+  },
 
- onReady(videoId, socket) {
-   let msgContainer = document.getElementById("msg-container")
-   let msgInput = document.getElementById("msg-input")
-   let postButton = document.getElementById("msg-submit")
-   let vidChannel = socket.channel("videos:" + videoId)
- }
+  onReady(videoId, socket) {
+    let msgContainer = document.getElementById("msg-container")
+    let msgInput = document.getElementById("msg-input")
+    let postButton = document.getElementById("msg-submit")
+    let vidChannel = socket.channel("videos:" + videoId)
+
+    vidChannel.join()
+      .receive("ok", resp => console.log("joined the video channel", resp))
+      .receive("error", reason => console.log("join failed", reason))
+  }
 }
 
 export default Video
